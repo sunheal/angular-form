029b04c403375f5115d2ba8df960566ac00a089a
sunheal <sunheal@gmail.com>
2022-10-08 01:20:32 -0400

    Cleanup and added CSS

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Subscribe to Our Services';
+  title: string = 'Subscribe to Armedia';
 
   constructor() { }
 
